55217696b07079bf6a47c7467709292713cb5a39
Mattymoo007 <mattymoo007@gmail.com>
2020-01-07 23:55:14 +0100

    solved issue in server

--- routes/api/users.js
+++ routes/api/users.js
@@ -33,7 +33,7 @@ router.post(
 			// MongoDB query
 			let user = await User.findOne({ email });
 			if (user) {
-				res.status(400).json({ errors: [ { msg: 'User already exists' } ] });
+				return res.status(400).json({ errors: [ { msg: 'User already exists' } ] });
 			}
 
 			// Get users Gravatar
