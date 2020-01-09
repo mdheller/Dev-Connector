9ec0967837b94307437822793d17a194e79ff78d
Mattymoo007 <Mattymoo007@gmail.com>
2020-01-09 13:21:49 +0100

    profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,46 @@
 const express = require('express');
-const router = express.Router();
 
-// @route GET api/profile
-// @desc  Test Route
-// @acces Public
-router.get('/', (req, res) => {
-	res.send('Profile route');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+const auth = require('../../middleware/auth');
+const User = require('../../models/Users');
+const Profile = require('../../models/Profile');
+
+// @route GET api/profile/me
+// @desc  Get current Users profile
+// @acces Private
+router.get('/me', auth, async (req, res) => {
+	try {
+		// Get user profile frome database
+		const profile = await Profile.findOne({ user: req.user.id }).populate('user', [
+			'name',
+			'avatar'
+		]);
+
+		if (!profile) {
+			return res.status(400).json({ msg: 'There is no profile for this user' });
+		}
+
+		res.json(profile);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 });
+
+// @route POST api/profile
+// @desc  Create or Update user profile
+// @acces Private
+router.post(
+	'/',
+	[
+		auth,
+		[
+			check('status', 'Status is required').not().isEmpty()
+		]
+	],
+	async (req, res) => {}
+);
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,43 +2,37 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
-const config = require('config');
 const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
-// User model
 const User = require('../../models/Users');
 
-// @route POST api/users
-// @desc  Register User
-// @acces Public
+// @route    POST api/users
+// @desc     Register user
+// @access   Public
 router.post(
 	'/',
 	[
-		check('name', 'A name is required').not().isEmpty(),
-		check('email', 'Please fill in a valid email').isEmail(),
-		check('password', 'Password must be at least 6 characters').isLength({ min: 6 })
+		check('name', 'Name is required').not().isEmpty(),
+		check('email', 'Please include a valid email').isEmail(),
+		check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
 	],
-	// Async with try catch block
 	async (req, res) => {
 		const errors = validationResult(req);
-
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
 		}
 
-		// Destructuring req.body
 		const { name, email, password } = req.body;
 
 		try {
-			// See if user exists
-			// MongoDB query
 			let user = await User.findOne({ email });
+
 			if (user) {
 				return res.status(400).json({ errors: [ { msg: 'User already exists' } ] });
 			}
 
-			// Get users Gravatar
 			const avatar = gravatar.url(email, {
 				s: '200',
 				r: 'pg',
@@ -52,23 +46,24 @@ router.post(
 				password
 			});
 
-			// Encrypt password
-			const salt = await bcrypt.genSalt();
+			const salt = await bcrypt.genSalt(10);
 
 			user.password = await bcrypt.hash(password, salt);
 
 			await user.save();
 
-			// Return JSON web token
-			const payload = { user: { id: user.id } };
-			jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 3600 }, (err, token) => {
+			const payload = {
+				user: {
+					id: user.id
+				}
+			};
+
+			jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
 				if (err) throw err;
 				res.json({ token });
 			});
-
-			// res.send('User registered');
 		} catch (err) {
-			console.log(err.message);
+			console.error(err.message);
 			res.status(500).send('Server error');
 		}
 	}
